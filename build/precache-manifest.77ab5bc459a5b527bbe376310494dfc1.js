6c7e3fafc65ec53a823d09aaf87e8f33deadbe45
mino9421 <mino9421@gmail.com>
2021-10-14 12:50:20 -0400

    redo

---
+++ build/precache-manifest.77ab5bc459a5b527bbe376310494dfc1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3874195f986746c7a7c1",
+    "url": "/mino9421/COMP3104/static/css/main.5e542085.chunk.css"
+  },
+  {
+    "revision": "3874195f986746c7a7c1",
+    "url": "/mino9421/COMP3104/static/js/main.3874195f.chunk.js"
+  },
+  {
+    "revision": "f75fbba95c9024e2abba",
+    "url": "/mino9421/COMP3104/static/js/runtime~main.f75fbba9.js"
+  },
+  {
+    "revision": "8ebec04f6c698faf83f8",
+    "url": "/mino9421/COMP3104/static/js/2.8ebec04f.chunk.js"
+  },
+  {
+    "revision": "cd905e5753a881f4219904ef3129606b",
+    "url": "/mino9421/COMP3104/index.html"
+  }
+];
---
+++ fileinfo.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# fileinfo.sh
+
+
+FILES = "/etc/syslog.conf
+/etc/ftpusers
+/etc/hosts
+/usr/bin/fakefile
+/usr/lib/log"
+
+
+
+
+exit 0 
